3cd669147adaa070febd38ec9622b8537e8b6381
robj <robert.jennings@gmail.com>
2015-05-28 08:10:11 +1000

    formatting

--- product.rb
+++ product.rb
@@ -2,6 +2,9 @@ class Product
 
   include Virtus.model(:strict => true)
 
+
+  # Currency should be handled with BigDecimal
+
   SALES_TAX_RATE =  BigDecimal('0.10')
   IMPORT_DUTY_TAX_RATE = BigDecimal('0.05')
   ROUND_TO_NEAREST_AMOUNT_IN_DOLLAR = BigDecimal('0.05')
@@ -16,6 +19,8 @@ class Product
   end 
 
 
+  # For testing purposes use a regex matcher to test for books, medical products and foods
+
   def is_sales_tax_exempt?
     return true if (self.name =~ /book/) || (self.name =~ /pills/) || (self.name =~ /chocolate/)
     false
@@ -28,13 +33,13 @@ class Product
 
 
   def import_duty_tax
-     self.is_imported? ? (self.price * IMPORT_DUTY_TAX_RATE) : ZERO
+    self.is_imported? ? (self.price * IMPORT_DUTY_TAX_RATE) : ZERO
   end
 
 
   def rounded_value(input)
-        number = (1 / ROUND_TO_NEAREST_AMOUNT_IN_DOLLAR)
-        (input*number).ceil/number
+    number = (1 / ROUND_TO_NEAREST_AMOUNT_IN_DOLLAR)
+    (input*number).ceil/number
   end
 
   def total_tax
